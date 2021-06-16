e0d2e4e0e15fca4f1bee5433b69263d7aa498915
Johnny-MetaTraderSolutions <xiaoqiqu.mt@gmail.com>
2021-06-16 16:54:41 +1000

    config and formatting

--- js/main.js
+++ js/main.js
@@ -9,11 +9,9 @@ document.addEventListener("keydown",(e)=>{
         $("#v").text(keys["v"]?"true":"false")
     }
     if(keys.r && keys.v){
-        console.log(document.getElementById("right-side-container").children)
         var elements = document.getElementById("right-side-container").children;
         
         Array.from(elements).forEach(element => {
-            console.log(element)
             element.style.display = "none"
           })
         document.getElementById("discription_of_finteza").style.display = "table-cell";
@@ -30,15 +28,10 @@ document.addEventListener("keyup",(e)=>{
     }
 })
 
-function title_height_change(){
-    var title = document.getElementById("website-title");
-    title.style.height = document.getElementById("logo-img").clientHeight+"px";
-    title.style.verticalAlign = "middle";
-}
+
 function form_generator() {
     var content = document.getElementById("discription_of_finteza");
     content.style.display = "none";
     var form = document.getElementById("211650936345860")
     form.style.display = "table-cell";
-    console.log(form)
 }
