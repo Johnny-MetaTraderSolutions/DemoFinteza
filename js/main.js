189a6dccd6aedf24a5b6aa38ea18e302e5290f68
Johnny-MetaTraderSolutions <xiaoqiqu.mt@gmail.com>
2021-06-18 09:24:39 +1000

    Revert "menu icon fix"
    This reverts commit d633957799ce25d4af8d148d7c88d0079f8b9765.
    
    # Conflicts:
    #	css/main.css
    #	index.html

--- js/main.js
+++ js/main.js
@@ -1,37 +1,40 @@
-var keys = {'r':false,'v':false}
-document.addEventListener("keydown",(e)=>{
-    if(e.key =="r"){
-        keys.r = true;
-        $("#r").text(keys["r"]?"true":"false")
-    }
-    if(e.key == "v"){
-        keys.v = true;
-        $("#v").text(keys["v"]?"true":"false")
-    }
-    if(keys.r && keys.v){
-        var elements = document.getElementById("right-side-container").children;
-        
-        Array.from(elements).forEach(element => {
-            element.style.display = "none"
-          })
-        document.getElementById("discription_of_finteza").style.display = "table-cell";
-    }
-})
-document.addEventListener("keyup",(e)=>{
-    if(e.key =="r"){
-        keys.r = false;
-        $("#r").text(keys["r"]?"true":"false")
-    }
-    if(e.key == "v"){
-        keys.v = false;
-        $("#v").text(keys["v"]?"true":"false")
-    }
-})
+var keys = { r: false, v: false };
+document.addEventListener("keydown", (e) => {
+  if (e.key == "r") {
+    keys.r = true;
+    $("#r").text(keys["r"] ? "true" : "false");
+  }
+  if (e.key == "v") {
+    keys.v = true;
+    $("#v").text(keys["v"] ? "true" : "false");
+  }
+  if (keys.r && keys.v) {
+    show_description();
+  }
+});
+document.addEventListener("keyup", (e) => {
+  if (e.key == "r") {
+    keys.r = false;
+    $("#r").text(keys["r"] ? "true" : "false");
+  }
+  if (e.key == "v") {
+    keys.v = false;
+    $("#v").text(keys["v"] ? "true" : "false");
+  }
+});
+function show_description() {
+  var elements = document.getElementById("right-side-container").children;
 
+  Array.from(elements).forEach((element) => {
+    element.style.display = "none";
+  });
+  document.getElementById("discription_of_finteza").style.display =
+    "table-cell";
+}
 
 function form_generator() {
-    var content = document.getElementById("discription_of_finteza");
-    content.style.display = "none";
-    var form = document.getElementById("211650936345860")
-    form.style.display = "table-cell";
+  var content = document.getElementById("discription_of_finteza");
+  content.style.display = "none";
+  var form = document.getElementById("211650936345860");
+  form.style.display = "table-cell";
 }
