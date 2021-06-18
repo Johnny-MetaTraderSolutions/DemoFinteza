d8b8517340b643dd19b1ec8b259a8de5a1f482d2
Johnny-MetaTraderSolutions <xiaoqiqu.mt@gmail.com>
2021-06-18 15:40:45 +1000

    Menu navigation panel added to the main page

--- js/main.js
+++ js/main.js
@@ -12,6 +12,10 @@ document.addEventListener("keydown", (e) => {
     show_description();
   }
 });
+document.addEventListener("click", function (event) {
+  if (!(myMenuPanel.contains(event.target) || menu.contains(event.target)))
+    closeNav();
+});
 document.addEventListener("keyup", (e) => {
   if (e.key == "r") {
     keys.r = false;
@@ -37,4 +41,13 @@ function form_generator() {
   content.style.display = "none";
   var form = document.getElementById("user-form");
   form.style.display = "table-cell";
+}
+function openNav() {
+  document.getElementById("myMenuPanel").style.width = "250px";
+  document.getElementById("myMenuPanel").style.height = "250px";
+}
+
+function closeNav() {
+  document.getElementById("myMenuPanel").style.width = "0";
+  document.getElementById("myMenuPanel").style.height = "0";
 }
